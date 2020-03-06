19848941d9b93ceffceecbcaf352eb0f7bed76d6
CalkaRob <calka.rob@gmail.com>
2020-03-06 12:00:51 +0100

    gh deploy

--- public/script.js
+++ public/script.js
@@ -236,6 +236,12 @@ d3.json("nodes-and-links.json", function(error, graph) {
 // Grouping nodes in a Force-Directed Graph
 // - https://bl.ocks.org/bumbeishvili/f027f1b6664d048e894d19e54feeed42
 
+// Collapsible Force Layout
+// - https://bl.ocks.org/mbostock/1093130
+
+//d3js forced network group hull collapse and expand
+// https://codepen.io/hadis-kia/pen/RwNWXje
+
 // check if two-way connection exists, then remove it
 // graph.links.forEach(function(link) {
 //   if (link.excluded !== undefined && link.excluded === true) {
