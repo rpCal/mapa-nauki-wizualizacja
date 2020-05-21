4c260d44eda790babe9a409ae3300ebfc0adde4e
CalkaRob <calka.rob@gmail.com>
2020-05-21 13:48:03 +0200

    Updates

--- script.js
+++ script.js
@@ -52,6 +52,12 @@ d3.json("nodes-and-links.json", function (error, graph) {
                 .duration(350)
                 .style("opacity", 0);
             }
+
+            svg
+              .selectAll(".links")
+              .transition()
+              .duration(350)
+              .style("opacity", interpolate(zoom_value_k, 0.9, 3, 0, 1));
           })
       )
       .append("g")
@@ -83,6 +89,7 @@ d3.json("nodes-and-links.json", function (error, graph) {
       .append("g")
       .attr("class", "links")
       .style("stroke", "#aaa")
+      .style("opacity", interpolate(0.9, 0.9, 3, 0, 1))
       .selectAll("line")
       .data(
         graph.links.filter(function (e) {
@@ -321,6 +328,57 @@ d3.json("nodes-and-links.json", function (error, graph) {
 
   run();
 });
+
+/**
+ * Returns a bezier interpolated value, using the given ranges
+ * @param {number} value  Value to be interpolated
+ * @param {number} s1 Source range start
+ * @param {number} s2  Source range end
+ * @param {number} t1  Target range start
+ * @param {number} t2  Target range end
+ * @param {number} [slope]  Weight of the curve (0.5 = linear, 0.1 = weighted near target start, 0.9 = weighted near target end)
+ * @returns {number} Interpolated value
+ */
+var interpolate = function (value, s1, s2, t1, t2, slope) {
+  //Default to linear interpolation
+  slope = slope || 0.5;
+
+  //If the value is out of the source range, floor to min/max target values
+  if (value < Math.min(s1, s2)) {
+    return Math.min(s1, s2) === s1 ? t1 : t2;
+  }
+
+  if (value > Math.max(s1, s2)) {
+    return Math.max(s1, s2) === s1 ? t1 : t2;
+  }
+
+  //Reverse the value, to make it correspond to the target range (this is a side-effect of the bezier calculation)
+  value = s2 - value;
+
+  var C1 = { x: s1, y: t1 }; //Start of bezier curve
+  var C3 = { x: s2, y: t2 }; //End of bezier curve
+  var C2 = {
+    //Control point
+    x: C3.x,
+    y: C1.y + Math.abs(slope) * (C3.y - C1.y),
+  };
+
+  //Find out how far the value is on the curve
+  var percent = value / (C3.x - C1.x);
+
+  return C1.y * b1(percent) + C2.y * b2(percent) + C3.y * b3(percent);
+
+  function b1(t) {
+    return t * t;
+  }
+  function b2(t) {
+    return 2 * t * (1 - t);
+  }
+  function b3(t) {
+    return (1 - t) * (1 - t);
+  }
+};
+
 // add for later
 // ------
 // Grouping nodes in a Force-Directed Graph
